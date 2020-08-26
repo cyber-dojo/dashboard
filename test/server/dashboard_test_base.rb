9456978afa9af94ccf7cfd68aeedf1ce925f25d6
Jon Jagger <jon@jaggersoft.com>
2020-08-26 21:05:44 +0100

    Add code/tests for dashboard_td_gapper

---
+++ test/server/dashboard_test_base.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require_relative '../id58_test_base'
+require_source 'app'
+require_source 'externals'
+
+class DashboardTestBase < Id58TestBase
+
+  include Rack::Test::Methods # [1]
+
+  def app # [1]
+    App.new(externals)
+  end
+
+  def externals
+    @externals ||= Externals.new
+  end
+
+end
