f8b2a9fecc9cefca810cae2cdb1011626be14915
Jon Jagger <jon@jaggersoft.com>
2020-08-27 20:36:30 +0100

    Add curl routes to demo.sh

--- demo.sh
+++ demo.sh
@@ -14,11 +14,11 @@ html_demo()
 {
   build_tagged_images
   containers_up api-demo
-  #api_demo
+  api_demo
   if [ "${1:-}" == '--no-browser' ]; then
     containers_down
   else
-    open "http://$(ip_address):${CYBER_DOJO_NGINX_PORT}/dashboard/show"
+    open "http://$(ip_address):$(port)/dashboard/show"
   fi
 }
 
@@ -27,26 +27,14 @@ api_demo()
 {
   echo
   echo API
-  curl_json_body_200 alive
-  curl_json_body_200 ready
-  curl_json_body_200 sha
+  curl_json_body_200 dashboard/alive
+  curl_json_body_200 dashboard/ready
+  curl_json_body_200 dashboard/sha
   echo
-  curl_200           assets/app.css 'Content-Type: text/css'
+  curl_200           dashboard/assets/app.css 'Content-Type: text/css'
+  curl_200           dashboard/assets/app.js 'Content-Type: application/javascript'
   echo
-  curl_200           group_custom_choose exercise
-  curl_200           kata_custom_choose exercise
-  echo
-  curl_params_302    group_custom_create "$(url_custom_params)"
-  curl_params_302    kata_custom_create  "$(url_custom_param)"
-  echo
-  curl_200           group_exercise_choose our
-  curl_200           kata_exercise_choose  my
-  echo
-  curl_200            group_language_choose our "$(url_exercise_param)"
-  curl_200            kata_language_choose  my  "$(url_exercise_param)"
-  echo
-  curl_url_params_302 group_language_create "$(url_exercise_param)" "$(url_languages_param)"
-  curl_url_params_302 kata_language_create  "$(url_exercise_param)" "$(url_language_param)"
+  curl_200           dashboard/show Hello
   echo
 }
 
@@ -73,8 +61,8 @@ curl_json_body_200()
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
 curl_200()
 {
-  local -r route="${1}"   # eg kata_choose
-  local -r pattern="${2}" # eg exercise
+  local -r route="${1}"   # eg dashboard/choose
+  local -r pattern="${2}" # eg Hello
   curl  \
     --fail \
     --request GET \
@@ -89,63 +77,10 @@ curl_200()
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
-curl_params_302()
-{
-  local -r route="${1}"  # eg kata_create
-  local -r params="${2}" # eg "display_name=Java Countdown, Round 1"
-  curl  \
-    --data-urlencode "${params}" \
-    --fail \
-    --request GET \
-    --silent \
-    --verbose \
-      "http://${IP_ADDRESS}:$(port)/${route}" \
-      > "$(log_filename)" 2>&1
-
-  grep --quiet 302 "$(log_filename)" # eg HTTP/1.1 302 Moved Temporarily
-  local -r result=$(grep Location "$(log_filename)" | head -n 1)
-  echo "$(tab)GET ${route} => 302 ${result}"
-}
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - -
-curl_url_params_302()
-{
-  local -r route="${1}"          # eg group_create
-  local -r exercise_param="${2}" # eg "exercise_name":"Fizz Buzz"
-  local -r language_param="${3}" # eg "languages_names":["Java, JUnit"]
-  curl  \
-    --data-urlencode "${exercise_param}" \
-    --data-urlencode "${language_param}" \
-    --fail \
-    --request GET \
-    --silent \
-    --verbose \
-      "http://${IP_ADDRESS}:$(port)/${route}" \
-      > "$(log_filename)" 2>&1
-
-  grep --quiet 302 "$(log_filename)" # eg HTTP/1.1 302 Moved Temporarily
-  local -r result=$(grep Location "$(log_filename)" | head -n 1)
-  echo "$(tab)GET ${route} => 302 ${result}"
-}
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - -
-port() { echo -n "${CYBER_DOJO_CREATOR_PORT}"; }
-
-url_custom_params() { url_param display_names[] "$(custom_name)"; }
-url_custom_param() { url_param display_name "$(custom_name)"; }
-custom_name() { echo -n 'Java Countdown, Round 1'; }
-
-url_exercise_param()  { url_param exercise_name "$(exercise_name)"; }
-exercise_name() { echo -n 'Fizz Buzz'; }
-
-url_languages_param() { url_param languages_names[] "$(language_name)"; }
-url_language_param()  { url_param language_name "$(language_name)"; }
-language_name() { echo -n 'Java, JUnit'; }
-
-url_param() { echo -n "${1}=${2}"; }
+port() { echo -n "${CYBER_DOJO_NGINX_PORT}"; }
 
 tab() { printf '\t'; }
-log_filename() { echo -n /tmp/creator.log; }
+log_filename() { echo -n /tmp/dashboard.log; }
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
 html_demo "$@"
