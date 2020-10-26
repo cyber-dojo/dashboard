4f7da3b07be4a509acff6ca2ffe79a6263f6ce65
Jon Jagger <jon@jaggersoft.com>
2020-10-26 15:34:40 +0000

    Refactoring; delete dead JS

--- source/server/code/assets/javascripts/hover_tips.js
+++ source/server/code/assets/javascripts/hover_tips.js
@@ -3,10 +3,10 @@
 (() => {
 
   cd.setupAvatarNameHoverTip = ($element, avatarIndex) => {
-    cd.setTip($element, () => {
+    setTip($element, () => {
       $.getJSON('/images/avatars/names.json', {}, (avatarsNames) => {
         const avatarName = avatarsNames[avatarIndex];
-        cd.showHoverTip($element, avatarName);
+        showHoverTip($element, avatarName);
       });
     });
   };
@@ -14,13 +14,13 @@
   // - - - - - - - - - - - - - - - - - - - -
 
   cd.setupTrafficLightTip = ($light, kataId, avatarIndex, wasIndex, nowIndex, colour, number) => {
-    cd.setTip($light, () => {
+    setTip($light, () => {
       const args = { id:kataId, was_index:wasIndex, now_index:nowIndex };
       // This should be $.getJSON but the receiving Rack
       // server currently reads JSON args from the request body.
       $.post('/differ/diff_summary', JSON.stringify(args), (data) => {
         const tip = tipHtml($light, avatarIndex, colour, number, data.diff_summary);
-        cd.showHoverTip($light, tip);
+        showHoverTip($light, tip);
       });
     });
   };
@@ -100,67 +100,26 @@
     }
     html += '</table>';
 
-    cd.setTip($count, () => {
-      cd.showHoverTip($count, html);
+    cd.createTip($count, html);
+  };
+
+  // - - - - - - - - - - - - - - - - - - - -
+
+  cd.createTip = (element, tip) => {
+    setTip(element, () => {
+      showHoverTip(element, tip);
     });
   };
 
   // - - - - - - - - - - - - - - - - - - - -
 
-  cd.setupHoverTips = function(nodes) { // TODO: DROP
-    nodes.each(function() {
-      const node = $(this);
-      const setTipCallBack = () => {
-        const tip = node.data('tip');
-        if (tip === 'traffic_light_count') {
-          cd.showHoverTip(node, trafficLightCountHoverTip(node));
-        } else {
-          cd.showHoverTip(node, tip);
-        }
-      };
-      cd.setTip(node, setTipCallBack);
-    });
-  };
-
-  // - - - - - - - - - - - - - - - - - - - -
-
-  const trafficLightCountHoverTip = (node) => {
-    // for dashboard avatar totalCount
-    const reds = node.data('red-count');
-    const ambers = node.data('amber-count');
-    const greens = node.data('green-count');
-    const timeOuts = node.data('timed-out-count');
-    const tr = (s) => `<tr>${s}</tr>`;
-    const td = (s) => `<td>${s}</td>`;
-    const trLight = (colour, count) => {
-      return tr(td('<img' +
-                   " class='traffic-light-diff-tip-traffic-light-image'" +
-                   ` src='/images/traffic-light/${colour}.png'>`) +
-                td(`<div class='traffic-light-diff-tip-tag ${colour}'>` +
-                   count +
-                   '</div>'));
-    };
-    let html = '';
-    html += '<table>';
-    html += trLight('red', reds);
-    html += trLight('amber', ambers);
-    html += trLight('green', greens);
-    if (timeOuts > 0) {
-      html += trLight('timed_out', timeOuts);
-    }
-    html += '</table>';
-    return html;
-  };
-
-  // - - - - - - - - - - - - - - - - - - - -
-
-  cd.setTip = (node, setTipCallBack) => {
-    // The speed of the mouse could easily exceed
+  const setTip = (node, setTipCallBack) => {
+    // The speed of the mouse could exceed
     // the speed of the getJSON callback...
     // The mouse-has-left attribute caters for this.
     node.mouseenter(() => {
       node.removeClass('mouse-has-left');
-      setTipCallBack(node);
+      setTipCallBack();
     });
     node.mouseleave(() => {
       node.addClass('mouse-has-left');
@@ -170,15 +129,7 @@
 
   // - - - - - - - - - - - - - - - - - - - -
 
-  cd.createTip = (element, tip) => {
-    cd.setTip(element, () => {
-      cd.showHoverTip(element, tip);
-    });
-  };
-
-  // - - - - - - - - - - - - - - - - - - - -
-
-  cd.showHoverTip = (node, tip) => {
+  const showHoverTip = (node, tip) => {
     if (!node.attr('disabled')) {
       if (!node.hasClass('mouse-has-left')) {
         const hoverTip = $('<div>', {
