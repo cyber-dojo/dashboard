8bcc8eddd1d6d19a6f77520aae7f4cada5ad27e9
Jon Jagger <jon@jaggersoft.com>
2020-11-27 12:40:35 +0000

    copy-in-test-data script needs less strict saver container name grep on ci.

--- sh/copy_in_saver_test_data.sh
+++ sh/copy_in_saver_test_data.sh
@@ -1,10 +1,9 @@
-#!/bin/bash -Eeu
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 copy_in_saver_test_data()
 {
   local -r SRC_PATH=${ROOT_DIR}/test/data/cyber-dojo
-  local -r SAVER_CID=$(docker ps --filter status=running --format '{{.Names}}' | grep "^dashboard_saver")
+  local -r SAVER_CID=$(docker ps --filter status=running --format '{{.Names}}' | grep "saver")
   local -r DEST_PATH=/cyber-dojo
   # You cannot docker cp to a tmpfs, so tar-piping instead...
   cd ${SRC_PATH} \
