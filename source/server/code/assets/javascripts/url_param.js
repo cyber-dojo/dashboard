2bc577537d530cc56a0e06d94eb9ace34da1e851
Jon Jagger <jon@jaggersoft.com>
2020-12-06 10:26:55 +0000

    Refactoring; tidied JS.

--- source/server/code/assets/javascripts/url_param.js
+++ source/server/code/assets/javascripts/url_param.js
@@ -3,6 +3,5 @@
 
 cd.urlParam = (name, fallBack) => {
   const params = new URLSearchParams(window.location.search);
-  const value = params.get(name) || fallBack;
-  return (value === null) ? fallBack : value.toString();
+  return params.get(name) || fallBack;
 };
