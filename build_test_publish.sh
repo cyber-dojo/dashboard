84bd03e2a4bf5aebd7e61930d0f5d3f595183722
Jon Jagger <jon@jaggersoft.com>
2021-03-27 09:35:59 +0000

    Add time whole script takes to run

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -7,6 +7,7 @@ source "${SH_DIR}/build_tagged_images.sh"
 source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up_healthy_and_clean.sh"
 source "${SH_DIR}/copy_in_saver_test_data.sh"
+source "${SH_DIR}/echo_seconds.sh"
 source "${SH_DIR}/exit_non_zero_unless_installed.sh"
 source "${SH_DIR}/exit_zero_if_build_only.sh"
 source "${SH_DIR}/exit_zero_if_show_help.sh"
@@ -19,6 +20,8 @@ source "${SH_DIR}/echo_versioner_env_vars.sh"
 export $(echo_versioner_env_vars)
 
 #- - - - - - - - - - - - - - - - - - - - - -
+t1=$(echo_seconds)
+
 exit_zero_if_show_help "$@"
 exit_non_zero_unless_installed docker
 exit_non_zero_unless_installed docker-compose
@@ -30,3 +33,6 @@ copy_in_saver_test_data
 test_in_containers "$@"
 containers_down
 on_ci_publish_images
+
+t2=$(echo_seconds)
+echo "Script took $(( t2-t1)) seconds"
