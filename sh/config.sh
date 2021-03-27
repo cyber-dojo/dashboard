72963a99f88dba81f7227388ee3b289cf0b30c0b
Jon Jagger <jon@jaggersoft.com>
2021-03-27 13:00:42 +0000

    Tidy config.sh file

--- sh/config.sh
+++ sh/config.sh
@@ -3,19 +3,13 @@
 export SERVICE_NAME=DASHBOARD
 export SERVICE_NAME_LOWER=$(echo "${SERVICE_NAME}" | tr '[:upper:]' '[:lower:]')
 
-# - - - - - - - - - - - - - - - - - - - - - - - -
+image_sha() { commit_sha; }
+image_tag() { image_sha | cut -c1-7; }
 
 commit_sha() { echo $(cd "${ROOT_DIR}" && git rev-parse HEAD); }
 
-image_sha() { commit_sha; }
-
-image_tag() { image_sha | cut -c1-7; }
-
-# - - - - - - - - - - - - - - - - - - - - - -
-
-# from versioner's output
-SERVER_IMAGE="CYBER_DOJO_${SERVICE_NAME}_IMAGE"
-SERVER_PORT="CYBER_DOJO_${SERVICE_NAME}_PORT"
+SERVER_IMAGE="CYBER_DOJO_${SERVICE_NAME}_IMAGE" # from cyberdojo/versioner
+SERVER_PORT="CYBER_DOJO_${SERVICE_NAME}_PORT"   # from cyberdojo/versioner
 
 server_image()     { echo "${!SERVER_IMAGE}"; }
 server_port()      { echo "${!SERVER_PORT}"; }
