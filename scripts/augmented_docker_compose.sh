cc11faec19b475ee44481ceda99ab44d09c370fe
Jon Jagger <jon@jaggersoft.com>
2021-03-27 20:39:58 +0000

    Add comment

--- scripts/augmented_docker_compose.sh
+++ scripts/augmented_docker_compose.sh
@@ -6,6 +6,9 @@
 # - - - - - - - - - - - - - - - - - - - - - -
 augmented_docker_compose()
 {
+  # When you run this you get a diagnostic message
+  #   Docker Compose is now in the Docker CLI, try `docker compose up`
+  # But that doesn't work. Currently, you still need docker-compose
   cd "${ROOT_DIR}" && cat "./docker-compose.yml" \
     | docker run --rm --interactive cyberdojo/service-yaml \
                          differ \
