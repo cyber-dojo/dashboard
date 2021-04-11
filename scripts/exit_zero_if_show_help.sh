446192433a385ec8c6197721e26462544c60eee8
Jon Jagger <jon@jaggersoft.com>
2021-04-11 08:11:59 +0100

    Tidy script

--- scripts/exit_zero_if_show_help.sh
+++ scripts/exit_zero_if_show_help.sh
@@ -8,10 +8,12 @@ exit_zero_if_show_help()
     echo
     echo "Use: ${MY_NAME} $(server_name) [ID...]"
     echo "Use: ${MY_NAME} $(client_name) [ID...]"
+    echo "Use: ${MY_NAME} -h|--help"
     echo 'Options:'
-    echo "   $(server_name)  - run the tests from inside the $(server_name)"
-    echo "   $(client_name)  - run the tests from inside the $(client_name)"
-    echo '   ID...   - only run the tests matching these identifiers'
+    echo "   $(server_name)      run the tests from inside the $(server_name)"
+    echo "   $(client_name)      run the tests from inside the $(client_name)"
+    echo '   ID...       only run the tests matching these identifiers'
+    echo '   -h|--help   show this help'
     echo
     exit 0
   fi
