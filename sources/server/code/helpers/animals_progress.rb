352ea55d90b7359a152096d6675e4ca1f76ce456
Jon Jagger <jon@jaggersoft.com>
2021-04-13 07:23:30 +0100

    Delete dead comments

--- sources/server/code/helpers/animals_progress.rb
+++ sources/server/code/helpers/animals_progress.rb
@@ -68,48 +68,7 @@ module AppHelpers # mixin
 
   def most_recent_progress(regexs, output)
     matches = regexs.map { |regex| Regexp.new(regex).match(output) }
-    {
-        text: matches.join,
-      colour: (matches[0] != nil ? 'red' : 'green')
-    }
+    matches.join
   end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - - - -
-=begin
-  def animals_progress2
-    # The original animals_progress function.
-    # Does not use external model service.
-    group.katas
-         .select(&:active?)
-         .map { |kata| animal_progress2(kata) }
-  end
-
-  def animal_progress2(kata)
-    {   colour: kata.lights[-1].colour,
-      progress: most_recent_progress2(kata),
-         index: kata.avatar_index,
-            id: kata.id
-    }
-  end
-
-  def most_recent_progress2(kata)
-    non_amber = kata.lights.reverse.find { |light|
-      [:red,:green].include?(light.colour)
-    }
-    if non_amber
-      output = non_amber.stdout['content'] + non_amber.stderr['content']
-    else
-      output = ''
-    end
-
-    regexs = kata.manifest.progress_regexs
-    matches = regexs.map { |regex| Regexp.new(regex).match(output) }
-
-    {
-        text: matches.join,
-      colour: (matches[0] != nil ? 'red' : 'green')
-    }
-  end
-=end
 
 end
