324513645c6b9d41c0eda9e4e97a36308b30a714
Jon Jagger <jon@jaggersoft.com>
2021-11-02 18:37:36 +0000

    Add missing env-vars to script.

--- scripts/echo_versioner_env_vars.sh
+++ scripts/echo_versioner_env_vars.sh
@@ -2,5 +2,8 @@
 # - - - - - - - - - - - - - - - - - - - - - - - -
 echo_versioner_env_vars()
 {
+  local -r sha="$(cd "${ROOT_DIR}" && git rev-parse HEAD)"
   docker run --rm cyberdojo/versioner
+  echo CYBER_DOJO_DASHBOARD_SHA="${sha}"
+  echo CYBER_DOJO_DASHBOARD_TAG="${sha:0:7}"
 }
