a050452ebd5bd5e90510e592e27b7bd44dc8a78c
JonJagger <jon@kosli.com>
2025-03-29 08:21:57 +0000

    Extend .snyk dates

--- bin/snyk_container_scan.sh
+++ bin/snyk_container_scan.sh
@@ -7,6 +7,5 @@ export $(echo_env_vars)
 exit_non_zero_unless_installed snyk
 
 snyk container test ${CYBER_DOJO_DASHBOARD_IMAGE}:${CYBER_DOJO_DASHBOARD_TAG} \
-      --file="${ROOT_DIR}/Dockerfile" \
       --json-file-output="${ROOT_DIR}/snyk.container.scan.json" \
       --policy-path="${ROOT_DIR}/.snyk"
