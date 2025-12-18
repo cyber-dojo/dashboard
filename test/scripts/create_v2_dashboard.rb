5ea8d39556c4da018934f0029d8179435b2e9784
JonJagger <jon@kosli.com>
2025-12-18 12:00:54 +0000

    Use predicted:'none'

--- test/scripts/create_v2_dashboard.rb
+++ test/scripts/create_v2_dashboard.rb
@@ -26,6 +26,7 @@ def create_v2_dashboard
   puts("Group ID=#{gid}")
   kid = saver.group_join(gid)
   puts("Kata ID=#{kid}")
+  # { index:0, event:created }
 
   files = manifest['visible_files']
   new_filename = 'wibble.txt'
@@ -33,12 +34,12 @@ def create_v2_dashboard
   files['cyber-dojo.sh']['content'] += '#comment'
   index = 1
   index = saver.kata_file_create(kid, index, files, filename=new_filename)
+  # { index:1, event:edit-file, filename:'cyber-dojo.sh'}
+  # { index:2, event:create-file, filename:'wibble.txt'}
 
   files[new_filename]['content'] += 'Hello world'
   index = saver.kata_file_switch(kid, index, files)
-
-  # index = saver.kata_file_delete(kid, index, files, filename)
-  # index = saver.kata_file_rename(kid, index, files, old_filename, new_filename)
+  # { index:3, event:edit-file, filename:'wibble.txt'}
 
   stdout = { 'content' => '', 'truncated' => false }
   stderr = { 'content' => '', 'truncated' => false }
@@ -46,15 +47,34 @@ def create_v2_dashboard
   summary = {
     duration: 1.234,
     colour: 'red',
-    predicted: '',
+    predicted: 'none',
     revert_if_wrong: false
   }
   index = saver.kata_ran_tests(kid, index, files, stdout, stderr, status, summary)
+  # { index:4, colour:red }
+
+  # index = saver.kata_file_delete(kid, index, files, filename)
+  # index = saver.kata_file_rename(kid, index, files, old_filename, new_filename)
 
   events = saver.kata_events(kid)
-
   puts(events)
+  # { index:0, event:created }
+  # { index:1, event:edit-file, filename:'cyber-dojo.sh'}
+  # { index:2, event:create-file, filename:'wibble.txt'}
+  # { index:3, event:edit-file, filename:'wibble.txt'}
+  # { index:4, colour:red }
+
   puts("http://localhost:80/dashboard/show/#{gid}?auto_refresh=false&minute_columns=true")
+
+  # The red traffic-light's tool-top gives plain red. Clicking it takes you to
+  # http://localhost/review/show/L5Gw6Y?was_index=3&now_index=4
+  # which is 3->4 which has no actual diff.
+  # Changing the URL to 
+  # http://localhost/review/show/L5Gw6Y?was_index=0&now_index=4
+  # gives the same
+  #
+  # So initial impressions look like differ is not working across the new non-test events.
+
 end
 
 create_v2_dashboard
