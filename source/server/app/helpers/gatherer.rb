c29fc7759a11597fe713e0a040fbf722f5937fdd
JonJagger <jon@kosli.com>
2026-01-30 15:30:49 +0000

    Add detailed? to gatherer

--- source/server/app/helpers/gatherer.rb
+++ source/server/app/helpers/gatherer.rb
@@ -17,7 +17,7 @@ module GathererHelper
       lights = []
       o['events'].each do |event|
         event = Event.new(event, previous_index)
-        if event.light?
+        if is_visible?(event)
           previous_index = event.index
           lights.append(event)
         end
@@ -34,6 +34,14 @@ module GathererHelper
     @gapped = gapper.fully_gapped(@all_lights, externals.time.now)
     @time_ticks = gapper.time_ticks(@gapped)
   end
+  
+  def is_visible?(event)
+    if detailed?
+      true
+    else
+      event.light?
+    end
+  end
 
   def seconds_per_column
     flag = params['minute_columns']
@@ -45,4 +53,9 @@ module GathererHelper
   def max_seconds_uncollapsed
     seconds_per_column * 5
   end
+
+  def detailed?
+    params['detailed'] == 'true'
+  end
+
 end
