9c6a791a1f03ae5ee4338c0c47dc3b8686acf5c2
JonJagger <jon@kosli.com>
2026-05-29 11:34:19 +0100

    Bump test metrics

--- test/server/check_test_metrics.rb
+++ test/server/check_test_metrics.rb
@@ -36,10 +36,10 @@ def table_data
     [ 'test.branches.total',   test_cov['branches']['total' ], '<=', 0   ],
     [ 'test.branches.missed',  test_cov['branches']['missed'], '<=', 0   ],
     [ nil ],
-    [ 'code.lines.total',      code_cov['lines'   ]['total' ], '<=', 401 ],
-    [ 'code.lines.missed',     code_cov['lines'   ]['missed'], '<=', 106 ],
-    [ 'code.branches.total',   code_cov['branches']['total' ], '<=', 50  ],
-    [ 'code.branches.missed',  code_cov['branches']['missed'], '<=', 24  ],
+    [ 'code.lines.total',      code_cov['lines'   ]['total' ], '<=', 406 ],
+    [ 'code.lines.missed',     code_cov['lines'   ]['missed'], '<=', 108 ],
+    [ 'code.branches.total',   code_cov['branches']['total' ], '<=', 54  ],
+    [ 'code.branches.missed',  code_cov['branches']['missed'], '<=', 26  ],
   ]
 end
 
